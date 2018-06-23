5c5b42d2200ade1b8c3229723a24050800371c7b
Lorefnon <lorefnon@gmail.com>
2018-06-23 21:24:57 +0530

    Make npm scripts cross-platform compatible

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const ghpages = require('gh-pages');
 
+if (process.env.NODE_ENV !== "production") {
+    throw new Error("Attempted to publish without setting production environment");
+}
+
 ghpages.publish(path.join(__dirname, '../dist-webpack'), function(err) {
     if (err) {
         console.error('Failed to publish');
