c9595fe3712e21274fea75ea613547bfd11ef0dc
Lorefnon <lorefnon@gmail.com>
2018-06-23 21:26:18 +0530

    Rename task: publish -> deploy for deploying to gh-pages

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,12 +2,12 @@ const path = require('path');
 const ghpages = require('gh-pages');
 
 if (process.env.NODE_ENV !== "production") {
-    throw new Error("Attempted to publish without setting production environment");
+    throw new Error("Attempted to deploy without setting production environment");
 }
 
 ghpages.publish(path.join(__dirname, '../dist-webpack'), function(err) {
     if (err) {
-        console.error('Failed to publish');
+        console.error('Failed to deploy');
     } else {
         console.log('Success !');
     }
